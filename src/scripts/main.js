e3cd18318f21517e65d73485e1713805127fcb1a
Christian Rebelsky <christian.rebelsky@gmail.com>
2017-12-29 12:38:02 +0100

    add editconfig and update eslint, add moment to control absence overlay

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,69 +1,73 @@
 import $ from 'jquery';
+import moment from 'moment';
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    function showOverlay(hash) {
-        const route = hash && hash.replace(/^#/, '');
-        if($('.overlay').hasClass('visible')) {
-            hideOverlay(route);
-            hideMenu();
-            return false;
-        } else {
-            $('.overlay__' + route).show();
-            $('.overlay').addClass('visible');
-        }
+  function showOverlay(hash) {
+    const route = hash && hash.replace(/^#/, '');
+    if ($('.overlay').hasClass('visible')) {
+      hideOverlay(route);
+      hideMenu();
+      return false;
+    } else {
+      $('.overlay__' + route).show();
+      $('.overlay').addClass('visible');
     }
+  }
 
-    function hideOverlay(route) {
-        $('.overlay__datenschutz, .overlay__impressum, .overlay__oeffnungszeiten').hide();
-        $('.overlay').removeClass('visible');
+  function hideOverlay() {
+    $('.overlay__datenschutz, .overlay__impressum, .overlay__oeffnungszeiten').hide();
+    $('.overlay').removeClass('visible');
+  }
+
+  function showMenu() {
+    if ($('#menu-toggle').hasClass('menu_toggle_active')) {
+      hideMenu();
+      $('#header').css('height', 'auto');
+      return false;
+    } else {
+      $('#menu-toggle').addClass('menu_toggle_active');
+      $('#header').css('height', '100%');
+      $('#header .show-for-large').css('display') === 'block' ? $('#header .menu').addClass('visible') : $('#header .mobilemenu').addClass('visible');
     }
+  }
 
-    function showMenu() {
-         if ($('#menu-toggle').hasClass('menu_toggle_active')) {
-            hideMenu();
-            $('#header').css('height', 'auto');
-            return false;
-        } else {
-            $('#menu-toggle').addClass('menu_toggle_active');
-            $('#header').css('height', '100%');
-            $('#header .show-for-large').css('display') === 'block' ? $('#header .menu').addClass('visible') : $('#header .mobilemenu').addClass('visible');
-        }
-    }
+  function showAwayNotice() {
+    showOverlay('notice');
+  }
 
-    function showAwayNotice() {
-      showOverlay('notice');
-    }
+  function hideMenu() {
+    $('#menu-toggle').removeClass('menu_toggle_active');
+    $('*').removeClass('menu__item--current');
+    $('#header .menu, #header .mobilemenu').removeClass('visible');
+  }
 
-    function hideMenu() {
-        $('#menu-toggle').removeClass('menu_toggle_active');
-        $('*').removeClass('menu__item--current');
-        $('#header .menu, #header .mobilemenu').removeClass('visible');
-    }
+  $('.menu__item').hover(function() {
+    $(this).addClass('menu__item--hover');
+  }, function() {
+    $(this).removeClass('menu__item--hover');
+  });
 
-    $('.menu__item').hover(function(){
-        $(this).addClass('menu__item--hover');
-    }, function(){
-        $(this).removeClass('menu__item--hover');
-    });
+  $('.menu__link').click(function(e) {
+    const hash = e.currentTarget.hash;
+    $(this).hasClass('overlay__link') && e.preventDefault();
+    $(this).hasClass('overlay__link') && showOverlay(hash);
+    $('*').removeClass('menu__item--current');
+    $(this).parent('.menu__item').addClass('menu__item--current');
+  });
 
-    $('.menu__link').click(function(e){
-        const hash = e.currentTarget.hash;
-        $(this).hasClass('overlay__link') && e.preventDefault();
-        $(this).hasClass('overlay__link') && showOverlay(hash);
-        $('*').removeClass('menu__item--current');
-        $(this).parent('.menu__item').addClass('menu__item--current');
-    });
+  $('#menu-toggle').click(function() {
+    showMenu();
+  });
 
-    $('#menu-toggle').click(function(){
-        showMenu();
-    });
+  $('#overlay-toggle').click(function() {
+    showOverlay();
+  });
 
-    $('#overlay-toggle').click(function(){
-        showOverlay();
-    });
-
-    $(window).on('load', function() {
+  $(window).on('load', function() {
+    const today = moment().format('YYYY-MM-DD');
+    if (moment(today).isBefore('2018-01-24', 'day')) {
       showAwayNotice();
-    });
+    }
+  });
 });
