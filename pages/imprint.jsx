dc6bc52663405f23ba994b61834e5db2d651e3b3
Christian Rebelsky <christian.rebelsky@gmail.com>
2018-03-07 17:09:09 +0100

    initial commit 1.0.0

---
+++ pages/imprint.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../layouts/Main';
+
+const ImprintPage = () => (
+  <Layout>
+    <p>Imprint context</p>
+  </Layout>
+);
+
+export default ImprintPage;
---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../layouts/Main';
+
+const IndexPage = () => (
+  <Layout>
+    <p>Children context</p>
+  </Layout>
+);
+
+export default IndexPage;
