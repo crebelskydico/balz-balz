39e263dff1d5a235b4b62041e7640aa49701eed0
Christian Rebelsky <christian.rebelsky@gmail.com>
2022-09-05 10:31:24 +0200

    fix typo and delete menu pdf

--- components/menu/main.tsx
+++ components/menu/main.tsx
@@ -7,7 +7,7 @@ type OptionalExceptFor<T, TRequired extends keyof T> = Partial<T> &
   Pick<T, TRequired>;
 
 const menuItems: OptionalExceptFor<HTMLAnchorElement, 'href' | 'title'>[] = [
-  { href: 'oeffnungszeiten', title: 'Öffnugszeiten' },
+  { href: 'oeffnungszeiten', title: 'Öffnungszeiten' },
   { href: 'impressum', title: 'Impressum' },
   { href: 'datenschutz', title: 'Datenschutz' },
   {
